27187384109552a6e19b85a23f92945923ba5776
Isaiah Peralta <iperalta@uri.edu>
2024-05-26 16:03:56 +0000

    Load config.env

--- src/bot.rb
+++ src/bot.rb
@@ -1,7 +1,7 @@
 require 'discordrb'
 require 'dotenv'
 
-Dotenv.load
+Dotenv.load('config.env')
 discord_bot_token = ENV['DISCORD_BOT_TOKEN']
 puts discord_bot_token
 bot = Discordrb::Bot.new token: discord_bot_token
