2fe3a382d721fa883bb3e98a2906f6c29e5cc9b5
Isaiah Peralta <iperalta@uri.edu>
2024-06-01 02:28:02 +0000

    feat: add log command

--- src/bot.rb
+++ src/bot.rb
@@ -1,13 +1,15 @@
+#!/usr/bin/env ruby
+
 require 'discordrb'
 require 'dotenv'
+require_relative 'commands/command'
 
 Dotenv.load('config.env')
 discord_bot_token = ENV['DISCORD_BOT_TOKEN']
 puts discord_bot_token
 bot = Discordrb::Bot.new token: discord_bot_token
 
-bot.message(content: 'Ping!') do |event|
-  event.respond 'Pong!'
-end
+bot =  Discordrb::Commands::CommandBot.new token: discord_bot_token, prefix: '/'
+commands = CraftminCommands.new(bot)
 
 bot.run
---
+++ src/commands/command.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'open3'
+
+class CraftminCommands
+  def initialize(bot)
+    @bot = bot
+    set_up
+  end
+
+  def log_command
+    @bot.command :log do |event|
+        log_file_path = "#{ENV['SERVER_INSTALLATION_DIR']}/server.log"
+        puts log_file_path
+        log_content = read_last_lines(log_file_path, 10)
+        unless @pinned_message_id
+          message = event.send_message("```\n#{log_content}\n```")
+          message.pin
+          @pinned_message_id = message.id
+          return 
+        end
+
+        begin
+          message = @bot.channel(event.channel.id).message(@pinned_message_id)
+          message.edit("```\n#{log_content}\n```")
+          rescue Discordrb::Errors::NoPermission
+            event.send_message("Error: Unable to edit the pinned message due to insufficient permissions.")
+          rescue Discordrb::Errors::NotFound
+            event.send_message("Error: Pinned message not found.")
+        end
+      end
+  end
+
+  def read_last_lines(file_path, n)
+    lines = File.readlines(file_path)
+    lines.last(n).join
+  rescue Errno::ENOENT
+    "Log file not found."
+  end
+
+    def set_up
+        log_command
+    end
+end
